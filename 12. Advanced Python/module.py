e9cf4d20e1ce3014afb1f3b89278c665b35bf580
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-18 12:44:18 +0530

    Significance of __name__ and Practice

---
+++ 12. Advanced Python/module.py
@@ -0,0 +1,10 @@
+def myFunc():
+    print("Hello, World!")
+
+
+
+
+if(__name__ == "__main__"): # If this code is executed by running the file directly
+      print("We are directly running this code")
+      myFunc()
+      print(__name__)
